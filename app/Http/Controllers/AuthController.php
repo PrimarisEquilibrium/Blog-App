c88e0753ce5349a474d26489d17efbf2fe667ace
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-12-11 17:08:43 -0500

    feat: create auth controller with login and register POST routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) 
+    {
+        $data = $request->validate([
+            "name" => ["required", "max:255"],
+            "email" => ["required", "email", "max:255", "unique:users"],
+            "password" => ["required", "confirmed"]
+        ]);
+
+        $user = User::create($data);
+
+        Auth::login($user);
+
+        return redirect()->route("home");
+    }
+
+    public function login(Request $request) 
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+ 
+            return redirect()->intended('dashboard');
+        }
+ 
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia("/", "Home")->name("home");
 
 Route::inertia("dashboard", "Dashboard")->name("dashboard");
-Route::inertia("login", "Auth/Login")->name("login");
+
 Route::inertia("register", "Auth/Register")->name("register");
+Route::post("register", [AuthController::class, "register"]);
+
+Route::inertia("login", "Auth/Login")->name("login");
+Route::post("login", [AuthController::class, "login"]);
 
 Route::resource("blogs", BlogController::class);
