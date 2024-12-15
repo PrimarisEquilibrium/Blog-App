1f08896d205685f3bcbff966e0deeb20b22a210e
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-12-14 20:55:12 -0500

    feat: add currently registered users name in dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,3 +1,13 @@
+<script setup>
+import { computed } from "vue";
+import { usePage } from "@inertiajs/vue3";
+
+const page = usePage();
+
+const user = computed(() => page.props.auth.user);
+</script>
+
 <template>
     <div>Dashboard Page</div>
+    <h1>Hello {{ user.name }}!</h1>
 </template>
