16e349fcf2056da609d4ff5c44203e3bcab013d1
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-12-14 20:36:30 -0500

    feat: complete login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,3 +1,40 @@
+<script setup>
+import { useForm } from "@inertiajs/vue3";
+
+const form = useForm({
+    email: null,
+    password: null,
+});
+
+const submit = () => {
+    form.post(route("login"), {
+        onError: () => {
+            form.reset("password");
+        },
+    });
+};
+</script>
+
 <template>
     <div>Login Page</div>
+    <form @submit.prevent="submit">
+        <div>
+            <label for="email">Email: </label>
+            <input type="email" name="email" id="email" v-model="form.email" />
+            <div v-if="form.errors.email">{{ form.errors.email }}</div>
+        </div>
+
+        <div>
+            <label for="password">Password: </label>
+            <input
+                type="password"
+                name="password"
+                id="password"
+                v-model="form.password"
+            />
+            <div v-if="form.errors.password">{{ form.errors.password }}</div>
+        </div>
+
+        <button>Submit</button>
+    </form>
 </template>
